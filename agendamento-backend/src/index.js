a70ff603de32331271bd2831b94972aa87661a49
Diana <diana45637@live.com>
2022-04-18 00:16:59 -0300

    Revert "feat: scheduling route"
    This reverts commit d3061ceba7d5652fd09b086942019b39c1fdeaa7.

--- agendamento-backend/src/index.js
+++ agendamento-backend/src/index.js
@@ -1,4 +1,5 @@
-import express from 'express';
+
+import express, { request, response } from 'express';
 import dotenv from 'dotenv';
 
 import ScheduleRouter from "./routes/ScheduleRouter.js"
@@ -9,8 +10,25 @@ const DATABASE_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT;
 
 const app = express();
+
 app.use(express.json());
 app.use("/api", ScheduleRouter);
+
+const schedule = [];
+
+app.get('/', (req, res, next) => {
+  res.json({message: "Tudo ok por aqui!", dados: schedule});
+})
+
+app.post("/schedule", (request, response, next) => {
+  console.log("Agendamento realizado");
+  //salvando os dados em memória
+  schedule.push({
+    nome: request.body.name,
+    idade: parseInt(request.body.age)
+  });
+  response.json({message: "Tudo certo", dados: schedule})
+});
 
 app.listen(PORT, () => {
   console.log(`Server Running on PORT ${PORT}`);
